fb44185dc299667cb5e36688b76c8f0e61f4ae82
malbwa <weldon0405@yahoo.com>
2018-08-01 05:31:23 -0700

    ToDoList complete with Delegate and Datasource methods

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Weldon Malbrough on 7/31/18.
+//  Copyright © 2018 Weldon Malbrough. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Find stuff", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    //MARK: - TableView DataSource Methods
+    /***************************************************************/
+    
+    //TODO: Declare cellForRowAtIndexPath here:
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    //TODO: Declare numberOfRowsInSection here:
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+        
+    }
+    
+    
+    //MARK: - TableView Delegate Methods
+    /***************************************************************/
+    
+    //TODO: UITableViewDelegate method:
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //TODO: UITableViewDataSource method:
+    
+}
+
+
